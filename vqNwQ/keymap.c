e0cf400e31630ebb2f958c21a991febe20563e92
Brunno Ferreira <git-commit@brunnoferreira.org>
2025-08-24 20:55:25 +1000

    feat: tap dance with macro for `->` on double tap

--- vqNwQ/keymap.c
+++ vqNwQ/keymap.c
@@ -290,7 +290,7 @@ void dance_2_finished(tap_dance_state_t *state, void *user_data) {
     dance_state[2].step = dance_step(state);
     switch (dance_state[2].step) {
         case SINGLE_TAP: register_code16(KC_MINUS); break;
-        case DOUBLE_TAP: register_code16(KC_RIGHT); break;
+        case DOUBLE_TAP: SEND_STRING(SS_TAP(X_MINUS)SS_DELAY(25)  SS_LSFT(SS_TAP(X_DOT))); break;        
         case DOUBLE_SINGLE_TAP: tap_code16(KC_MINUS); register_code16(KC_MINUS);
     }
 }
@@ -299,7 +299,7 @@ void dance_2_reset(tap_dance_state_t *state, void *user_data) {
     wait_ms(10);
     switch (dance_state[2].step) {
         case SINGLE_TAP: unregister_code16(KC_MINUS); break;
-        case DOUBLE_TAP: unregister_code16(KC_RIGHT); break;
+        case DOUBLE_TAP: break;
         case DOUBLE_SINGLE_TAP: unregister_code16(KC_MINUS); break;
     }
     dance_state[2].step = 0;
